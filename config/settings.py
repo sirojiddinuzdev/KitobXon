818423e57d436170dc1413ec5c6e4dfe6e894c1d
sirojiddin <ysirojiddin007@gmail.com>
2026-06-07 15:42:45 +0500

    the end

--- config/settings.py
+++ config/settings.py
@@ -154,3 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CSRF_TRUSTED_ORIGINS = [
+    'https://kitobhon.uz',
+    'https://www.kitobhon.uz',
+]
